2f8d72a2791a5c93b9754987cfbdfdd557612513
Pittinan Saeyang <pittinansaeyang@gmail.com>
2025-04-24 04:55:25 +0700

    ex03 ing

--- module00/lib/ex03/ex03.dart
+++ module00/lib/ex03/ex03.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:module00/ex03/calculator_screen.dart';
 
 class Ex03 extends StatelessWidget {
   const Ex03({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Exercise 03', style: TextStyle(fontSize: 24)),
-    );
+    return Theme(
+      data: ThemeData.dark(),
+      child: const CalculatorScreen());
+     // Calls the UI screen
   }
 }
--- module00/lib/ex03/main.dart
+++ module00/lib/ex03/main.dart
@@ -1,21 +1,21 @@
-// import 'package:ex02/calculator_screen.dart';
-// import 'package:flutter/material.dart';
+import 'package:module00/ex03/calculator_screen.dart';
+import 'package:flutter/material.dart';
 
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Calculator',
-//       theme: ThemeData.dark(),
-//       home: const CalculatorScreen(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
 
