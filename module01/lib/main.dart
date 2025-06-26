7bb5fad5f8493fc667b3c3829200e8bd8af7fe82
Pittinan Saeyang <pittinansaeyang@gmail.com>
2025-06-27 03:39:18 +0700

    module01

---
+++ module01/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'ex00/ex00.dart';
+import 'ex01/ex01.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  Widget getExercise(int index) {
+    switch (index) {
+      case 0:
+        return const Ex00();
+      case 1:
+        return const Ex01();
+      default:
+        throw Exception('Invalid exercise index');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: getExercise(0),
+      // home: getExercise(1),
+    );
+  }
+}
