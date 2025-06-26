213138307c5a369533123f40d23674f89e685cfb
Pittinan Saeyang <pittinansaeyang@gmail.com>
2025-06-27 03:38:42 +0700

    ex01 - add file

---
+++ module01/lib/ex01/ex01.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Ex01 extends StatelessWidget {
+  const Ex01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exercise 01'),
+      ),
+    );
+  }
+}
